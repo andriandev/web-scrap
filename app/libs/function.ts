abf67630e73240920b9d48cb78afb49ddd84ebcc
andriandev <andrianpoint@gmail.com>
2025-05-22 11:12:26 +0700

    First commit

---
+++ app/libs/function.ts
@@ -0,0 +1,110 @@
+import fs from 'fs';
+import path from 'path';
+import sharp from 'sharp';
+import * as cheerio from 'cheerio';
+
+function fetchOption({ url = '' }) {
+  return {
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+      Referer: url,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  };
+}
+
+export async function fetchHTML(url: string): Promise<cheerio.CheerioAPI> {
+  const res = await fetch(url, fetchOption({ url: url }));
+  const html = await res.text();
+  return cheerio.load(html);
+}
+
+export async function openLinks(
+  baseUrl: string,
+  selector: string,
+  callback: (url: string, $: cheerio.CheerioAPI) => Promise<void>
+) {
+  const $ = await fetchHTML(baseUrl);
+
+  const links = $(selector)
+    .map((_, el) => $(el).attr('href'))
+    .get()
+    .filter(Boolean);
+
+  for (const link of links) {
+    const fullUrl = new URL(link!, baseUrl).href;
+    const page$ = await fetchHTML(fullUrl);
+    await callback(fullUrl, page$);
+  }
+}
+
+export function extractImageInfo(url: string) {
+  const regex = /\/(\d{4})\/(\d{2})\/(\d+)_/;
+  const match = url.match(regex);
+
+  if (!match)
+    return {
+      year: '2000',
+      month: '01',
+      timestamp: '946684800',
+    };
+
+  const [, year, month, timestamp] = match;
+  return {
+    year,
+    month,
+    timestamp,
+  };
+}
+
+export function extractYouTubeId(url: string): string | null {
+  const match = url.match(/\/embed\/([a-zA-Z0-9_-]{11})/);
+  return match ? match[1] : null;
+}
+
+export function generatePageUrls(
+  baseUrl: string,
+  start: number,
+  end: number
+): string[] {
+  if (!baseUrl?.trim())
+    throw new Error('Baseurl is required and cannot be empty');
+  if (start < 1) throw new Error('Start page must be greater than 0');
+  if (end < start) throw new Error('End page cannot be less than start page');
+
+  const normalizedBaseUrl = baseUrl.endsWith('/') ? baseUrl : `${baseUrl}/`;
+
+  return Array.from({ length: end - start + 1 }, (_, index) => {
+    const currentPage = start + index;
+    return currentPage === 1
+      ? normalizedBaseUrl
+      : `${normalizedBaseUrl}page/${currentPage}/`;
+  });
+}
+
+export async function downloadImage(
+  url: string,
+  folderPath: string
+): Promise<string> {
+  const res = await fetch(url, fetchOption({ url }));
+  if (!res.ok) throw new Error(`Failed to fetch ${url}`);
+
+  const buffer = await res.arrayBuffer();
+
+  fs.mkdirSync(folderPath, { recursive: true });
+
+  const imgFilename = url.split('/').pop() || 'image';
+  const filenameWithoutExt = path.parse(imgFilename).name;
+  const outputPath = path.join(folderPath, filenameWithoutExt + '.webp');
+
+  await sharp(Buffer.from(buffer)).webp({ quality: 50 }).toFile(outputPath);
+
+  return filenameWithoutExt + '.webp';
+}
+
+export function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
---
+++ app/web/drays.ts
@@ -0,0 +1,111 @@
+import fs from 'fs';
+import path from 'path';
+import sanitize from 'sanitize-filename';
+import chalk from 'chalk';
+import {
+  openLinks,
+  extractImageInfo,
+  extractYouTubeId,
+  generatePageUrls,
+  downloadImage,
+  sleep,
+} from '../libs/function';
+
+const baseUrl = process.env.BASE_URL || '';
+const start = Number(process.env.START) || 1;
+const end = Number(process.env.END) || 2;
+
+const listUrl = generatePageUrls(baseUrl, start, end);
+const log = console.log;
+
+for (const link of listUrl) {
+  log(chalk.cyan('Visit:', link));
+  await openLinks(link, '#movies .content a', async (url, $) => {
+    const img = $('.posthumb img').attr('src') || '';
+    const info = extractImageInfo(img);
+    let type: string;
+
+    if (baseUrl.includes('/movies/')) {
+      type = 'movies';
+    } else if (baseUrl.includes('/series/')) {
+      type = 'series';
+    } else {
+      type = 'post';
+    }
+
+    const folderPath = path.join(
+      process.cwd(),
+      'data',
+      type,
+      info.year,
+      info.month
+    );
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath, { recursive: true });
+    }
+
+    const imgFolderPath = path.join(
+      process.cwd(),
+      'data',
+      'img',
+      info.year,
+      info.month
+    );
+    const imgUpload = await downloadImage(img, imgFolderPath);
+
+    const pathname = new URL(url).pathname;
+    const filename = `${sanitize(pathname)}.json`;
+    const filepath = path.join(folderPath, filename);
+
+    const items = $('.postdetail .thn .mr-4');
+    const ytSrc = $('#tab-2.tab-content iframe').attr('src');
+    const linkHtml = `<div id="dl_tab">${$('#dl_tab').html()}</div>`;
+
+    const title = $('.postdetail h1').text().trim();
+    const imgPost = `/${info.year}/${info.month}/${imgUpload}`;
+    const imgLarge = $('.backdrop img').attr('src');
+    const year = $(items[0]).text().trim();
+    const country = $(items[1]).text().trim();
+    const duration = $(items[2]).text().trim();
+    const rate = $('.backdrop span.bg-yellow-105').text().trim() || '6';
+    const quality = $('.backdrop span.bg-orange-500').text().trim() || 'BluRay';
+    const genres = $('.postdetail .info p.mt-3 a')
+      .map((_, el) => $(el).text().trim())
+      .get();
+    const sinopsis = $('#tab-1.tab-content').html();
+    const trailer = ytSrc ? extractYouTubeId(ytSrc) : null;
+    const link_dl = linkHtml
+      .replaceAll('\n', '')
+      .replace(
+        ' class="text-center bg-blue-500 rounded font-medium text-white py-2 block"',
+        ''
+      )
+      .replaceAll(' class="flex flex-wrap my-2 mb-3"', '')
+      .replaceAll(
+        ' bg-blue-500 px-2 py-1 rounded text-center text-white mr-2',
+        ''
+      );
+
+    const data = {
+      title,
+      slug: sanitize(pathname),
+      img: imgPost,
+      img_large: imgLarge,
+      duration,
+      year,
+      country,
+      trailer,
+      rate,
+      quality,
+      genres,
+      sinopsis: sinopsis ? sinopsis.replaceAll('\n', '').trim() : null,
+      link_dl,
+      date: info.timestamp,
+    };
+
+    fs.writeFileSync(filepath, JSON.stringify(data, null, 2));
+    log(chalk.green('Scraped:', url));
+    await sleep(1000);
+  });
+  await sleep(3000);
+}
